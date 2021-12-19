526c7ec81ab18b4d0223ce66c47ace64fb41bfbf
Lijitsu <Lijitsu@gmail.com>
2021-12-19 23:04:22 +0000

    Finalized basic user view + tiny adminview update

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,16 @@ Rails.application.routes.draw do
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  resources :series
+  resources :categories
+  resources :games
+  resources :levels
+  resources :resources
+  resources :skips
+  resources :tricks
+  resources :guides
+  resources :game_tricks
+
   namespace :admin do
     resources :series
     resources :categories
