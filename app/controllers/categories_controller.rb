559c29cb83677fb0a027518c20da33028665473f
Lijitsu <Lijitsu@gmail.com>
2022-01-29 19:25:29 +0000

    Made category pages split up by game

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,10 @@
+# The public view controller for categories. Doesn't require things like CRUD, just viewing.
 class CategoriesController < ApplicationController
+  # The duplicated code here (from the games controller) is needed to separate the categories by game
+  # on the category index page.
   def index
-    @categories = Category.all
+    @categories = Category.all.includes(:game)
+    @games = Game.all
   end
 
   def show
@@ -11,5 +15,9 @@ class CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(:name, :overview, :platform, :rules, :game_id)
+  end
+
+  def game_params
+    params.require(:game).permit(:name, :platform, :release_date, :series_id, :overview, game_tricks_attributes: [:trick_id])
   end
 end
