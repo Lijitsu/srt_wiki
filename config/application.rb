b4dbe420a1b7a66c3fbc96c017aa2b58cea41a1f
Lijitsu <Lijitsu@gmail.com>
2022-02-12 12:11:32 +0000

    Rails update for security

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative "boot"
 
 require "rails/all"
 
-
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -12,11 +10,7 @@ module SrtWiki
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.generators do |g|
-      g.stylesheets = false
-      g.javascripts = false
-      g.helper = false
-    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
